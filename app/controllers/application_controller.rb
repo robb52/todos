b35c679c7e03bad43a0d607f27cfa06b85c0e4a4
Rob Brown <brown4homes@yahoo.com>
2018-12-11 12:39:43 -0800

    Extract session[:cuurent_email] to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,12 @@ class ApplicationController < ActionController::Base
     session[:current_email].present?
   end
 
+  def current_email
+    session[:current_email]
+  end
+
+  def sing_in_as(email)
+    session[:current_email] = email
+  end
+
 end
